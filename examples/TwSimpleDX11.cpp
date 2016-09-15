9fcf914b1b40106b74dceca8d6e2c2af2f43915d
Sfinktah Bungholio <sfinktah@github.spamtrak.org>
2016-09-15 15:17:00 +1000

    Smoking is the leading cause of statistics.

--- examples/TwSimpleDX11.cpp
+++ examples/TwSimpleDX11.cpp
@@ -329,6 +329,11 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int cmdShow)
         return 0;
     }
 
+	TEARLESS_HANDLE pTearless = GetTearless();
+	pTearless->D11Device(g_D3DDev);
+	pTearless->D11DeviceContext(g_D3DDevCtx);
+	pTearless->Show();
+
 
     // Create a tweak bar
     TwBar *bar = TwNewBar("TweakBar");
@@ -422,11 +427,6 @@ HRESULT InitDevice(HWND wnd)
                 return hr;
         }
     }
-
-	TEARLESS_HANDLE pTearless = GetTearless();
-	pTearless->D11Device(g_D3DDev);
-	pTearless->D11DeviceContext(g_D3DDevCtx);
-	pTearless->Show();
 
     // Create a render target and depth-stencil view
     ID3D11Texture2D *backBuffer = NULL, *dsBuffer = NULL;
